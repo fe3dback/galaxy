cf80ed61da3a6d9538a8a70a688e4493b85e45a8
fe3dback <fe3dback@yandex.ru>
2020-08-19 23:31:57 +0300

    wip collisions

---
+++ engine/collision/ray.go
@@ -0,0 +1,11 @@
+package collision
+
+import "github.com/fe3dback/galaxy/engine"
+
+func Ray2Rect(ray engine.Line, r engine.Rect) bool {
+	panic("Ray2Rect")
+}
+
+func Ray2Circle(ray engine.Line, c engine.Circle) bool {
+	panic("Ray2Circle")
+}
